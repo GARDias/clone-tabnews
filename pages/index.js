af19439e726779e0d6d37d71cd50929f3af24859
Gabriel Dias <gabrielapdias0@gmail.com>
2025-11-23 18:55:03 -0300

    Só documentando quando eu estudei sobre testes automatizados

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>
-        Agora to vendo continuos deploying, vai da bom mano, to evoluindo!
-      </h1>
+      <h1>Aprendi como funciona testes automatizados! Vamooooo</h1>
       <img src="/assets/images/dance.gif" />
     </div>
   );
