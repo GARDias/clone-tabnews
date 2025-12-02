b70182cb1a8f3c8f9777c78fe985d61482d631ed
Gabriel Dias <gabrielapdias0@gmail.com>
2025-12-02 09:39:14 -0300

    add `databse.js` and `.env` files

--- infra/database.js
+++ infra/database.js
@@ -1,11 +1,11 @@
 import { Client } from "pg";
 async function query(queryObject) {
   const client = new Client({
-    user: "postgres",
-    password: "Console.log0",
-    host: "localhost",
-    port: "5432",
-    database: "postgres",
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTEGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
   });
   await client.connect();
 
