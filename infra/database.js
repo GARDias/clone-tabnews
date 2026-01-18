95c89a39ec331f60849d879a5981a2cbcdbe8035
Gabriel Dias <gabrielapdias0@gmail.com>
2026-01-18 16:58:56 -0300

    Adicionando o neon serveless ao projeto

--- infra/database.js
+++ infra/database.js
@@ -1,12 +1,6 @@
 import { Client } from "pg";
 async function query(queryObject) {
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTEGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-  });
+  const client = new Client( process.env.DATABASE_URL);
   try{
     await client.connect();
     const res = await client.query(queryObject);
