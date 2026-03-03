c923c09f3e87828887884d0325e50847e4bb06d8
Gabriel Dias <gabrielapdias0@gmail.com>
2026-03-03 12:22:53 +0000

    hotfix

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -31,5 +31,5 @@ export default async function migrations(request, response) {
     return response.status(200).json(afterMigrated);
   }
 
-  return response.status(405).json();
+  return response.status(405).end();
 }
